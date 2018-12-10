f33cb5d439ae02de58f42e7b96e0f2d44e6cc3e2
nanachi772 <noranko2539@gmail.com>
2018-12-10 22:23:06 +0000

    Third commit

--- foo/test/programming/sample_test.rb
+++ foo/test/programming/sample_test.rb
@@ -1,8 +1,7 @@
 require 'minitest/autorun'
 
 class SampleTest < Minitest::test
-	def test_sample
-		assert_equal 'RUBY','ruby'.upcase
-	end
-
+  def test_sample
+    assert_equal 'RUBY','ruby'.upcase
+  end
 end
